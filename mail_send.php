96a0263b0d4b415d7305ecef5f457f4697011c59
gutocoelho <augustoufc@hotmail.com>
2020-09-16 12:16:10 -0300

    ajuste email

--- mail_send.php
+++ mail_send.php
@@ -1,36 +1,7 @@
 <?php
 
-function pegaValor($valor) {
-    return isset($_POST[$valor]) ? $_POST[$valor] : '';
-}
+require_once 'src/PHPMailer.php';
 
-function validaEmail($email) {
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
-}
-
-function enviaEmail($de, $assunto, $para, $email_servidor) {
-    $headers = "From: $email_servidor\r\n" .
-               "Reply-To: $de\r\n" .
-               "X-Mailer: PHP/" . phpversion() . "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-  
-  mail($para, $assunto, $headers);
-}
-
-$email_servidor = "contato@mygeekbox.com.br";
-$para = "contato@mygeekbox.com.br";
-$de = pegaValor("email");
-$assunto = "Estou interessado na My Geek Box";
-
-if ($nome && validaEmail($de)) {
-    enviaEmail($de, $assunto, $para, $email_servidor);
-    $pagina = "index.php";
-
-} else {
-    $pagina = "mail_error.php";
-}
-
-header("location:$pagina");
+//inserir exemplo
 
 ?>
